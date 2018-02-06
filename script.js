7dfb563767c9b8f4e4e9c6d440cfbc862307361a
Tania Buzykina <tania8137@gmail.com>
2018-02-06 05:47:39 +0200

    add a little bit of code for ViewModule dropdown menu

--- script.js
+++ script.js
@@ -6,7 +6,24 @@ $(document).ready(function () {
     $(".mainSearch").focus(
         function(){
             $(this).val(" ");
+           // $(".filterList").show();
         }
     );
+/*
+    $(".mainSearch").blur(
+        function(){
+            $(".filterList").hide();
+        }
+    );*/
 
+    $(".aViewModuleDropDown").click(
+        function(){
+            $(".viewModule > .dd-checkbox").show();
+        }
+    );
+    $(".aViewModuleDropDown").dblclick(
+        function(){
+            $(".viewModule > .dd-checkbox").hide();
+        }
+    );
 });
