5b48d5287a3315962639fe6bb87f0a4d9f161d3a
ZheniaFIT <zhen0077@ukr.net>
2018-02-06 20:46:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 $(document).ready(function () {
+/*
+    $(".search").click(function () {
+        $(".dd-checkbox").toggleClass("visible");       //не работает
+    });*/
     $(".mainSearch").focus(function(){
         $(".filterList").show();
         $(".search").css("border-bottom", "1px solid lightgrey");
     });
-
+    /*$(".search").blur(function(){
+        $(".filterList").hide();
+    });*/
     $(".filterList").click(function(){
         $(".search > .dd-checkbox").toggleClass("becomeVisible");
     });
@@ -38,12 +44,9 @@ $(document).ready(function () {
         }
     });
 
-    $( ".mainSearch" ).keypress(function( event ) {
-        if ( event.which == 13 ) {
-           let $querylog = $(".mainSearch").val();
-           console.log($querylog);
-        }
-      });
+    $(".profile-add").click(function () {
+        $(".drop-add").toggleClass("show-block");
+    });
 
 });
 
