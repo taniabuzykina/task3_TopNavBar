337f94bcf3bb55a281149ccdebf089a664c2c622
Tania Buzykina <tania8137@gmail.com>
2018-02-06 22:30:03 +0200

    update

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
-$(document).ready(function () {
-/*
-    $(".search").click(function () {
-        $(".dd-checkbox").toggleClass("visible");       //не работает
-    });*/
+/* $(document).ready(function () {
     $(".mainSearch").focus(function(){
         $(".filterList").show();
         $(".search").css("border-bottom", "1px solid lightgrey");
     });
-    /*$(".search").blur(function(){
-        $(".filterList").hide();
-    });*/
     $(".filterList").click(function(){
         $(".search > .dd-checkbox").toggleClass("becomeVisible");
     });
@@ -62,11 +55,78 @@ $(document).ready(function () {
                     $arr.push($(this).text());
                 });
 
+            console.log($arr);
+            console.log($querylog);
+            let obj = new Obj($querylog, $arr);
+            console.log(obj);
+        }
+    });
+
+});
+
+ */
+$(document).ready(function () {
+
+    $(".mainSearch").focus(function(){
+        $(".filterList").show();
+        $(".search").css("border-bottom", "1px solid lightgrey");
+    });
+
+    //region CHECKBOX DROPDOWN
+    $(".filterList").click(function(){
+        $(".search > .dd-checkbox").toggleClass("becomeVisible");
+    });
+    //endregion
+
+    let $view_module = $(".viewModule .dd-checkbox");
+    $(".aViewModuleDropDown").click(function(){
+        if($view_module.hasClass('open')) 
+            $view_module.removeClass('open');
+        else 
+            $view_module.addClass('open');
+    });
+
+    $(".moduleMenuOption").click( function () {
+        var col = $(this).find(".material-icons").css('color');
+        $("#abc").css("color", col);
+        $view_module.removeClass('open');
+    });
+
+    $(".option").click(function(){
+        let $tick = $(this).find(".material-icons.tick");
+        let $box = $(this).find(".option-box");
+        let $text = $(this).find("p");
+
+        $tick.toggleClass("checked");
+        $box.toggleClass("box-checked");
+        $text.toggleClass("p-checked");
+
+        // console.log($text.text());
+    });
+
+    function Obj(query, options) {
+        this.query = query;
+        this.options = options;
+    }
+
+    $( ".search" ).keypress(function( event ) {
+        if ( event.which == 13 ) {
+            let $querylog = $(".mainSearch").val();
+            let $arr = [];
+            let $text = $(this).find(".p-checked")
+                .map(function () {
+                    $arr.push($(this).text());
+                });
+
             // console.log($arr);
             // console.log($querylog);
             let obj = new Obj($querylog, $arr);
             console.log(obj);
         }
+    });
+
+    $(".profile-add").click(function () {
+        $(".drop-add").toggleClass("show-block");
     });
 
 });
